5a56f716821fa84c732a87d8ce83b473fea5daca
shadialtarsha <shadi.altarsha.94@gmail.com>
2018-02-21 14:47:09 +0200

    Init commit

---
+++ server/server.js
@@ -0,0 +1,11 @@
+require('./../config/config');
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.use(express.static(path.join(__dirname, '../public/')));
+
+app.listen(process.env.PORT, () => {
+    console.log('Server is running....', process.env.PORT);
+});
