63a1a049ee45c11ec91acdf3918329475a991ca5
shadialtarsha <shadi.altarsha.94@gmail.com>
2018-02-21 20:45:56 +0200

    Emit newMessage on createMessage

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -2,15 +2,9 @@ const socket = io();
 
 socket.on('connect', () => {
     console.log('connected to the server');
-});
-
-socket.on('newMessage', (body) => {
-    console.log('New message:', body);
-});
-
-socket.emit('createMessage', {
-    from: "shadi@exmaple.com",
-    text: "Hey, missed u"
+    socket.on('newMessage', (body) => {
+        console.log('New message:', body);
+    });
 });
 
 socket.on('disconnect', () => {
--- server/server.js
+++ server/server.js
@@ -14,14 +14,13 @@ app.use(express.static(path.join(__dirname, '../public/')));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('newMessage', {
-        from: 'elie@example.com',
-        text: 'Hey, what\'s up ',
-        createdAt: 123
-    });
-
     socket.on('createMessage', (message) => {
         console.log(`Create a new Message: ${JSON.stringify(message)}`);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        })
     });
 
     socket.on('disconnect', () => {
