e34400fa41ab75bfc6fc859c235d1547874279fe
shadialtarsha <shadi.altarsha.94@gmail.com>
2018-02-21 21:08:33 +0200

    Greet new user, and alert others

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -2,8 +2,9 @@ const socket = io();
 
 socket.on('connect', () => {
     console.log('connected to the server');
-    socket.on('newMessage', (body) => {
-        console.log('New message:', body);
+
+    socket.on('newMessage', (message) => {
+        console.log('New message:', message);
     });
 });
 
--- server/server.js
+++ server/server.js
@@ -12,15 +12,28 @@ const io = socketIO(server);
 app.use(express.static(path.join(__dirname, '../public/')));
 
 io.on('connection', (socket) => {
-    console.log('New user connected');
+
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user has joined',
+        createdAt: new Date().getTime()
+    });
 
     socket.on('createMessage', (message) => {
         console.log(`Create a new Message: ${JSON.stringify(message)}`);
+
         io.emit('newMessage', {
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
-        })
+        });
+
     });
 
     socket.on('disconnect', () => {
