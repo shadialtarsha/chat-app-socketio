d47c2c8c855537b975a694c98961eb1aa22cdf06
shadialtarsha <shadi.altarsha.94@gmail.com>
2018-02-23 16:23:41 +0200

    Fix bug

--- server/server.js
+++ server/server.js
@@ -46,7 +46,7 @@ io.on('connection', (socket) => {
     socket.on('createLocationMessage', (coords) => {
         const user = users.getUser(socket.id);
         if (user) {
-            io.emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
+            io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
         }
     });
 
